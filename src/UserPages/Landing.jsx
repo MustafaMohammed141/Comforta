58358950935eaa3c336cf01296ab2dbfda3e523a
Mustafa <mostafamohamed12434@gmail.com>
2025-04-29 12:15:49 +0300

    Created main pages, installed material tailwind and handled routing

---
+++ src/UserPages/Landing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Featured from "./components/Featured";
+import Footer from "./components/Footer";
+
+const Landing = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Featured />
+      <Footer />
+    </div>
+  );
+};
+
+export default Landing;
