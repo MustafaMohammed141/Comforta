4e6f71abf997dc8605b9a7df54ec3ed1948a90a9
Mustafa <mostafamohamed12434@gmail.com>
2025-05-01 16:34:42 +0300

    - Removed Header and Dashboard components from AdminPages. - Added UsersDB component with loading state and user table. - Integrated user fetching logic in App component. - Created ManageUsers component for user details and updates. - Added Loading component for better user experience during data fetching. - Updated package.json to include react-loading-indicators dependency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./UserPages/Landing";
-import Dashboard from "./AdminPages/Dashboard";
+import Dashboard from "./AdminPages/Pages/Dashboard";
 import Products from "./UserPages/Products";
 import CartPage from "./UserPages/CartPage";
 import Login from "./UserPages/Login";
 import SignUp from "./UserPages/SignUp";
 import Contact from "./UserPages/Contact";
 import About from "./UserPages/About";
-
+import axios from "axios";
+import Loading from "./UserPages/components/Loading";
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getUsers = async () => {
+    const VITE_DB = import.meta.env.VITE_DB;
+
+    try {
+      const req = await axios({
+        url: `${VITE_DB}/users`,
+        method: "get",
+      });
+      setUsers(req.data);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-50 text-gray-800">
       <Routes>
@@ -21,7 +42,16 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/contactus" element={<Contact />} />
         <Route path="/aboutus" element={<About />} />
-        <Route path="/admindb/*" element={<Dashboard />} />
+        <Route
+          path="/admindb/*"
+          element={
+            <Dashboard
+              users={users}
+              isLoading={isLoading}
+              refreshUsers={getUsers}
+            />
+          }
+        />
       </Routes>
     </div>
   );
